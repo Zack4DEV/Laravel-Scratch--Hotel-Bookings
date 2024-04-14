144629bc57ef5ac25f94befef6b2b98a72e7158c
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2024-04-14 13:01:29 +0000

    origin

--- replit.nix
+++ replit.nix
@@ -1,8 +1,14 @@
 { pkgs }: {
 	deps = [
+<<<<<<< HEAD
     pkgs.sqlite.bin
 		pkgs.nodejs-18_x
    	pkgs.php82Packages.composer
+=======
+		pkgs.sqlite.bin
+		pkgs.nodejs-18_x
+		pkgs.php82Packages.composer
+>>>>>>> 0d57709 (origin)
 		pkgs.php82
 	];
 }
